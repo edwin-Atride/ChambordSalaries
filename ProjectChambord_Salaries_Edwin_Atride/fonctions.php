223c8c722df5415e8f0a86494f0b6790162d2542
Edwin <atride.edwin@gmail.com>
2026-02-20 11:47:32 +0100

    Clean up

--- ProjectChambord_Salaries_Edwin_Atride/fonctions.php
+++ ProjectChambord_Salaries_Edwin_Atride/fonctions.php
@@ -2,13 +2,10 @@
 require_once("connexion.php");
 function getLesContacts(){
 try {
-/*Sélectionne toutes les valeurs dans la table contact*/
 global $conn ;
 $sql = "SELECT * FROM salaries";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-/*Retourne un tableau associatif pour chaque entrée de notre table
-*avec le nom des colonnes sélectionnées en clefs*/
 $resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
 return $resultat ;
 }
@@ -20,13 +17,10 @@ echo "Erreur : " . $e->getMessage();
 
 function NbSalaries(){
 try {
-/*Sélectionne toutes les valeurs dans la table contact*/
 global $conn ;
 $sql = "SELECT COUNT(`prenom`) As NbSalarier FROM salaries";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-/*Retourne un tableau associatif pour chaque entrée de notre table
-*avec le nom des colonnes sélectionnées en clefs*/
 $resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
 return $resultat ;
 }
@@ -37,13 +31,10 @@ echo "Erreur : " . $e->getMessage();
 
 function Salairemoyen(){
 try {
-/*Sélectionne toutes les valeurs dans la table contact*/
 global $conn ;
 $sql = "SELECT AVG(`salaire`) As MoyenneSalaire FROM salaries";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-/*Retourne un tableau associatif pour chaque entrée de notre table
-*avec le nom des colonnes sélectionnées en clefs*/
 $resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
 return $resultat ;
 }
@@ -54,13 +45,10 @@ echo "Erreur : " . $e->getMessage();
 
 function SalaireMAXMIN(){
 try {
-/*Sélectionne toutes les valeurs dans la table contact*/
 global $conn ;
 $sql = "SELECT MAX(`salaire`) AS MAXSALAIRE, MIN(`salaire`) AS MINSALAIRE FROM salaries";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-/*Retourne un tableau associatif pour chaque entrée de notre table
-*avec le nom des colonnes sélectionnées en clefs*/
 $resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
 return $resultat ;
 }
@@ -71,13 +59,10 @@ echo "Erreur : " . $e->getMessage();
 
 function NbSalariesService(){
 try {
-/*Sélectionne toutes les valeurs dans la table contact*/
 global $conn ;
 $sql = "SELECT COUNT(`prenom`) AS nombre, `service` FROM salaries GROUP BY `service`";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-/*Retourne un tableau associatif pour chaque entrée de notre table
-*avec le nom des colonnes sélectionnées en clefs*/
 $resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
 return $resultat ;
 }

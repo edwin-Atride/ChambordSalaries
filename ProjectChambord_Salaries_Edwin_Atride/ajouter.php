261fe030c69e0d114f88d87e8077a4d498c613fb
Edwin <atride.edwin@gmail.com>
2026-02-20 11:49:47 +0100

    Clean up

--- ProjectChambord_Salaries_Edwin_Atride/ajouter.php
+++ ProjectChambord_Salaries_Edwin_Atride/ajouter.php
@@ -65,15 +65,11 @@
         $password = 'sio';
         $dbname = "chambord";
 
-        //On essaie de se connecter
         try{
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            //On définit le mode d'erreur de PDO sur Exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            //echo 'Connexion réussie';
         }
 
-        /*On capture les exceptions si une exception est lancée et on affiche *les informations relatives à celle-ci*/
         catch(PDOException $e){
             echo "Erreur : " . $e->getMessage();
         }

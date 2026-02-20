09b4b3d3ed8e21aad3db0ede6173837caefe4959
Edwin <atride.edwin@gmail.com>
2026-02-20 11:48:41 +0100

    Clean up

--- ProjectChambord_Salaries_Edwin_Atride/delete.php
+++ ProjectChambord_Salaries_Edwin_Atride/delete.php
@@ -7,22 +7,17 @@
       $password = 'sio';
       $dbname = "chambord";
             
-     //On essaie de se connecter
       try{
           $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-          //On définit le mode d'erreur de PDO sur Exception
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           echo 'Connexion réussie';
       }
             
-    /*On capture les exceptions si une exception est lancée et on affiche
-    *les informations relatives à celle-ci*/
     catch(PDOException $e){
          echo "Erreur : " . $e->getMessage();
     }
 
     try { 
-      /*Sélectionne toutes les valeurs dans la table contact*/
       $sql = "delete FROM salaries where id=$id";
       $conn->exec($sql);
      header('Location: listeSalaries.php');
